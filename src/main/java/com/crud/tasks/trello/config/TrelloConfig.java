b5759895193007bdb1902d2137ae93773b0aa52e
Rafal Glodek <rafal.glodek@interia.pl>
2023-01-06 19:57:01 +0000

    jr war

--- src/main/java/com/crud/tasks/trello/config/TrelloConfig.java
+++ src/main/java/com/crud/tasks/trello/config/TrelloConfig.java
@@ -1,10 +1,12 @@
 package com.crud.tasks.trello.config;
 
+import lombok.Data;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Getter
+@Data
 @Component
 public class TrelloConfig {
 
